607697151f3e6ce757fd8fea76d8dfe1f8f81873
ergo9ine <ergo9ine@gmail.com>
2020-10-22 10:54:45 +0900

    test

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# gsmap
+# gsmap-r(https://gsmap.github.io/gsmap-r)
+
+「gsmap-r (Genshin Impact collection navigator project)」 is licensed under the MIT License
+
+## Note
+- If you want to participate, please PR.
+
+# Thanks
+- [gsmap](https://yuanshen.site) - Inspiration and motivation.
--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 t = L.latLngBounds([0, 0], [-66.5, 90]);
-var map = L.map("map", {
+var raw,data,map = L.map("map", {
 	//crs: L.CRS.Simple,
 	center: [-35, 45],
 	zoomDelta: 0.5,
@@ -11,6 +11,9 @@ var map = L.map("map", {
 	attributionControl: false,
 	zoomControl: false
 });
+L.control.attribution({
+	prefix: "<a href='https://github.com/gsmap/gsmap-r' target='_blank'>github</a>"
+}).addTo(map);
 L.control.zoom({
 	zoomInTitle: '+',
 	zoomOutTitle: '-'
@@ -25,8 +28,26 @@ L.tileLayer.t = function () {
 return new L.TileLayer.T();
 }
 map.addLayer(L.tileLayer.t());
-var redMarker = L.AwesomeMarkers.icon({
-	icon: 'bahai',
-	markerColor: 'red'
-}); 
-L.marker(["-20.64306554672647", "43.09936523437501"], {icon: redMarker}).addTo(map);
+
+fetch("js/coord.json").then(response => response.json()
+	.then(table => {
+		raw =  table,data = table.data
+		drawlayer("바람신의 눈","bahai","green")
+	})
+);
+function drawlayer(category,symbol,color) {
+	for (const e in data[category]) {
+		var id = data[category][e].id,
+			coord_x = data[category][e].geometry.coordinates[0],
+			coord_y = data[category][e].geometry.coordinates[1]
+		console.log(coord_y)
+		L.marker(
+			[coord_x, coord_y],
+			{icon: L.AwesomeMarkers.icon({
+				icon: symbol,
+				markerColor: color,
+				className: `awesome-marker mark_${category}_${id}`
+			})}
+		).addTo(map).bindPopup(`${category} - ${id}`)
+	}
+}
