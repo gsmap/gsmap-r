0f81096de4a8a12a0b792dd5e618534344308de7
ergo9ine <ergo9ine@gmail.com>
2020-10-23 10:48:16 +0900

    fullscreen, non-tile map

--- js/index.js
+++ js/index.js
@@ -8,33 +8,35 @@ var raw, data, map = L.map("map", {
 	zoom: 5,
 	maxBounds: t,
 	attributionControl: false,
-	zoomControl: false
+	fullscreenControl: true,
+	fullscreenControlOptions: {
+		position: 'topleft'
+	}
 });
 L.control.attribution({
 	prefix: "<a href='https://github.com/gsmap/gsmap-r' target='_blank'>github</a>"
 }).addTo(map);
-L.control.zoom({
-	zoomInTitle: '+',
-	zoomOutTitle: '-'
-}).addTo(map);
+var imageUrl = 'https://cdn.jsdelivr.net/gh/gsmap/gsmap_tiles@1.1/tiles/low_teyvat.webp',
+	imageBounds = [[0, 0], [-66.5, -90]];
+L.imageOverlay(imageUrl, t).addTo(map);
+/* old
 L.TileLayer.T = L.TileLayer.extend({
 	getTileUrl: function (coords) {
-		return `https://cdn.jsdelivr.net/gh/gsmap/gsmap_tiles@1.0/tiles/${coords.z}/${coords.x}_${coords.y}.webp`;
+		return `https://cdn.jsdelivr.net/gh/gsmap/gsmap_tiles@1.1/tiles/${coords.z}/${coords.x}_${coords.y}.webp`;
 	},
 	reuseTiles: true
 });
-/*
 L.TileLayer.T = L.TileLayer.extend({
 	getTileUrl: function (coords) {
 	return `tiles/${coords.z}/${coords.x}_${coords.y}.webp`;
 	},
 	reuseTiles: true
 });
-*/
 L.tileLayer.t = function () {
 	return new L.TileLayer.T()
 };
 map.addLayer(L.tileLayer.t());
+*/
 fetch("js/coord.json").then(response => response.json()
 	.then(table => {
 		raw = table, data = table.data
@@ -54,12 +56,16 @@ function drawlayer(category, symbol, color) {
 			[coord_x, coord_y],
 			{
 				icon: L.AwesomeMarkers.icon({
+					shadowAnchor: [0, 0],
+					shadowSize: [0, 0],
 					icon: symbol,
 					markerColor: color,
-					className: `awesome-marker ${category}_${id} ${opacity}`
+					className: `awesome-marker ${cache} ${opacity}`
 				})
 			}
 		).addTo(map).bindPopup(`
+		${cache}
+		<br>
 		<label class="switch">
 			<input type="checkbox">
 			<span class="slider"></span>
@@ -67,9 +73,9 @@ function drawlayer(category, symbol, color) {
 		`)
 	}
 };
-var marker, switch_c;
 map.on("popupopen", function (e) {
 	var id = e.popup._source._icon.classList[2];
+	//console.log(id.split("_"))
 	if (localStorage[id]) {
 		e.target._popup._container.querySelector("input").checked = true
 	};
